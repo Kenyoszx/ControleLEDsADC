33ef985760366fc721eb5096731cd15415ad32fc
João Victor de Souza Silva <joao1452019@gmail.com>
2025-02-11 15:51:21 -0300

    Inicialização dos pinos e leitura joystick

--- Joystick.c
+++ Joystick.c
@@ -1,4 +1,4 @@
-//Inclusão das Bibliotecas
+// Inclusão das Bibliotecas
 #include <stdio.h>
 #include <stdlib.h>
 #include "pico/stdlib.h"
@@ -7,64 +7,71 @@
 #include "lib/ssd1306.h"
 #include "lib/font.h"
 
-//Definição dos Pinos
-#define I2C_PORT i2c1
+// Definição dos Pinos
+
 #define I2C_SDA 14
 #define I2C_SCL 15
+#define JOYSTICK_X_PIN 26 // GPIO para eixo X
+#define JOYSTICK_Y_PIN 27 // GPIO para eixo Y
+#define JOYSTICK_PB 22    // GPIO para botão do Joystick
+#define Botao_A 5         // GPIO para botão A
+
+//Constantes
+#define I2C_PORT i2c1
 #define endereco 0x3C
-#define JOYSTICK_X_PIN 26  // GPIO para eixo X
-#define JOYSTICK_Y_PIN 27  // GPIO para eixo Y
-#define JOYSTICK_PB 22 // GPIO para botão do Joystick
-#define Botao_A 5 // GPIO para botão A
+#define WRAP_PERIOD 20000
+#define PWM_DIVISOR 125.0
+#define MOTOR_STEP 5
 
-void init();
-
+//Protótipos das Funções
+refresh_led_state(uint16_t x,uint16_t y);
 
 int main()
 {
     stdio_init_all();
+    gpio_init(JOYSTICK_PB);
+    gpio_set_dir(JOYSTICK_PB, GPIO_IN);
+    gpio_pull_up(JOYSTICK_PB);
+
+    gpio_init(Botao_A);
+    gpio_set_dir(Botao_A, GPIO_IN);
+    gpio_pull_up(Botao_A);
 
     // I2C Initialisation. Using it at 400Khz.
-    i2c_init(I2C_PORT, 400*1000);
-    
-    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
-    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C_SDA);
-    gpio_pull_up(I2C_SCL);
-   
+    i2c_init(I2C_PORT, 400 * 1000);
 
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);                    // Set the GPIO pin function to I2C
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);                    // Set the GPIO pin function to I2C
+    gpio_pull_up(I2C_SDA);                                        // Pull up the data line
+    gpio_pull_up(I2C_SCL);                                        // Pull up the clock line
+    ssd1306_t ssd;                                                // Inicializa a estrutura do display
+    ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT); // Inicializa o display
+    ssd1306_config(&ssd);                                         // Configura o display
+    ssd1306_send_data(&ssd);                                      // Envia os dados para o display
+
+    // Limpa o display. O display inicia com todos os pixels apagados.
+    ssd1306_fill(&ssd, false);
+    ssd1306_send_data(&ssd);
+
+    adc_init();
+    adc_gpio_init(JOYSTICK_X_PIN);
+    adc_gpio_init(JOYSTICK_Y_PIN);
+
+    uint16_t adc_value_x;
+    uint16_t adc_value_y;
+
+    bool cor = true;
+    while (true)
+    {
+        adc_select_input(0); // Seleciona o ADC para eixo X. O pino 26 como entrada analógica
+        adc_value_x = adc_read();
+        adc_select_input(1); // Seleciona o ADC para eixo Y. O pino 27 como entrada analógica
+        adc_value_y = adc_read();
+        refresh_led_state(adc_value_x,adc_value_y);
+        sleep_ms(100);
     }
 }
 
-void init(){
-  gpio_init(JOYSTICK_PB);
-  gpio_set_dir(JOYSTICK_PB, GPIO_IN);
-  gpio_pull_up(JOYSTICK_PB); 
+refresh_led_state(uint16_t x,uint16_t y){
 
-  gpio_init(Botao_A);
-  gpio_set_dir(Botao_A, GPIO_IN);
-  gpio_pull_up(Botao_A);
-
-  // I2C Initialisation. Using it at 400Khz.
-  i2c_init(I2C_PORT, 400 * 1000);
-
-  gpio_set_function(I2C_SDA, GPIO_FUNC_I2C); // Set the GPIO pin function to I2C
-  gpio_set_function(I2C_SCL, GPIO_FUNC_I2C); // Set the GPIO pin function to I2C
-  gpio_pull_up(I2C_SDA); // Pull up the data line
-  gpio_pull_up(I2C_SCL); // Pull up the clock line
-  ssd1306_t ssd; // Inicializa a estrutura do display
-  ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT); // Inicializa o display
-  ssd1306_config(&ssd); // Configura o display
-  ssd1306_send_data(&ssd); // Envia os dados para o display
-
-  // Limpa o display. O display inicia com todos os pixels apagados.
-  ssd1306_fill(&ssd, false);
-  ssd1306_send_data(&ssd);
-
-  adc_init();
-  adc_gpio_init(JOYSTICK_X_PIN);
-  adc_gpio_init(JOYSTICK_Y_PIN);  
 }
