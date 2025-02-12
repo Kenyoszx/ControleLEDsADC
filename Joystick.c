22e549efbcd22f4899d0022ea49966b9317c814e
João Victor de Souza Silva <joao1452019@gmail.com>
2025-02-11 21:09:23 -0300

    PWM

--- Joystick.c
+++ Joystick.c
@@ -6,6 +6,8 @@
 #include "hardware/i2c.h"
 #include "lib/ssd1306.h"
 #include "lib/font.h"
+#include "hardware/pwm.h"
+#include "hardware/clocks.h"
 
 // Definição dos Pinos
 
@@ -22,22 +24,73 @@
 //Constantes
 #define I2C_PORT i2c1
 #define endereco 0x3C
-#define WRAP_PERIOD 20000
-#define PWM_DIVISOR 125.0
-#define MOTOR_STEP 5
+#define WRAP_PERIOD 4096
+#define PWM_DIVISOR 254.6
+#define STEP 5
 
 //Variáveis Globais
 static volatile uint32_t last_time = 0; // Armazena o tempo do último evento (em microssegundos)
 
 //Protótipos das Funções
-refresh_led_state(uint16_t x,uint16_t y);
-uint init_PWM_Red_LED();
-uint init_PWM_Blue_LED();
-void wrapHandlerRED();
-void wrapHandlerBLUE();
+void init();
+void refresh_led_state_X(uint16_t x);
+void refresh_led_state_Y(uint16_t y);
+static void gpio_irq_handler(uint gpio, uint32_t events);
+uint pwm_init_gpio(uint gpio, uint wrap);
+
 int main()
 {
-    stdio_init_all();
+    stdio_init_all(); //inicialização entrada e saída 
+    
+    // Variáveis
+    uint16_t adc_value_x;
+    uint16_t adc_value_y;
+    uint slice_red = pwm_init_gpio(LED_PIN_RED, WRAP_PERIOD);
+    uint slice_blue = pwm_init_gpio(LED_PIN_BLUE, WRAP_PERIOD);
+    bool cor = true;
+
+    init(); // Inicialização dos Pinos
+
+    //Inicialização do I2C
+
+    i2c_init(I2C_PORT, 400 * 1000);
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);                    // Set the GPIO pin function to I2C
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);                    // Set the GPIO pin function to I2C
+    gpio_pull_up(I2C_SDA);                                        // Pull up the data line
+    gpio_pull_up(I2C_SCL);                                        // Pull up the clock line
+    ssd1306_t ssd;                                                // Inicializa a estrutura do display
+    ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT); // Inicializa o display
+    ssd1306_config(&ssd);                                         // Configura o display
+    ssd1306_send_data(&ssd);                                      // Envia os dados para o display
+    ssd1306_fill(&ssd, false); // Limpa o display. O display inicia com todos os pixels apagados.
+    ssd1306_send_data(&ssd);
+
+    //Inicialização ADC
+    adc_init();
+    adc_gpio_init(JOYSTICK_X_PIN);
+    adc_gpio_init(JOYSTICK_Y_PIN);
+    
+
+    while (true)
+    {
+        adc_select_input(0); // Seleciona o ADC para eixo X. O pino 26 como entrada analógica
+        adc_value_x = adc_read();
+        adc_select_input(1); // Seleciona o ADC para eixo Y. O pino 27 como entrada analógica
+        adc_value_y = adc_read();
+        refresh_led_state_X(adc_value_x);
+        refresh_led_state_Y(adc_value_y);
+
+        sleep_ms(100);
+    }
+}
+
+void refresh_led_state_X(uint16_t x){
+    
+}
+void refresh_led_state_Y(uint16_t y){
+
+}
+void init(){
     gpio_init(JOYSTICK_PB);
     gpio_set_dir(JOYSTICK_PB, GPIO_IN);
     gpio_pull_up(JOYSTICK_PB);
@@ -53,42 +106,6 @@ int main()
     gpio_set_dir(LED_PIN_GREEN, GPIO_OUT);
     gpio_put(LED_PIN_GREEN, 0);
 
-    i2c_init(I2C_PORT, 400 * 1000);
-
-    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);                    // Set the GPIO pin function to I2C
-    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);                    // Set the GPIO pin function to I2C
-    gpio_pull_up(I2C_SDA);                                        // Pull up the data line
-    gpio_pull_up(I2C_SCL);                                        // Pull up the clock line
-    ssd1306_t ssd;                                                // Inicializa a estrutura do display
-    ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT); // Inicializa o display
-    ssd1306_config(&ssd);                                         // Configura o display
-    ssd1306_send_data(&ssd);                                      // Envia os dados para o display
-
-    // Limpa o display. O display inicia com todos os pixels apagados.
-    ssd1306_fill(&ssd, false);
-    ssd1306_send_data(&ssd);
-
-    adc_init();
-    adc_gpio_init(JOYSTICK_X_PIN);
-    adc_gpio_init(JOYSTICK_Y_PIN);
-
-    uint16_t adc_value_x;
-    uint16_t adc_value_y;
-
-    bool cor = true;
-    while (true)
-    {
-        adc_select_input(0); // Seleciona o ADC para eixo X. O pino 26 como entrada analógica
-        adc_value_x = adc_read();
-        adc_select_input(1); // Seleciona o ADC para eixo Y. O pino 27 como entrada analógica
-        adc_value_y = adc_read();
-        refresh_led_state(adc_value_x,adc_value_y);
-        sleep_ms(100);
-    }
-}
-
-refresh_led_state(uint16_t x,uint16_t y){
-
 }
 static void gpio_irq_handler(uint gpio, uint32_t events)
 {
@@ -98,7 +115,7 @@ static void gpio_irq_handler(uint gpio, uint32_t events)
     uint32_t current_time = to_us_since_boot(get_absolute_time());
 
     // Verifica se passou tempo suficiente desde o último evento
-    if (current_time - last_time > 50000) // 49 ms de debouncing
+    if (current_time - last_time > 200000) // 200 ms de debouncing
     {
         last_time = current_time; // Atualiza o tempo do último evento
         // Código Função:
@@ -108,41 +125,18 @@ static void gpio_irq_handler(uint gpio, uint32_t events)
         }
         else if (gpio == JOYSTICK_PB)
         {
-            
+            bool state = gpio_get(LED_PIN_GREEN);
+            gpio_put(LED_PIN_GREEN,!state);
+            printf("Joystick\n");
         }
     }
 }
-uint init_PWM_Red_LED(){
-    // inicializa o pwm do ServoMotor
+uint pwm_init_gpio(uint gpio, uint wrap) {
+    gpio_set_function(gpio, GPIO_FUNC_PWM);
 
-    gpio_set_function(LED_PIN_RED, GPIO_FUNC_PWM);        // habilitar o pino GPIO como PWM
-    uint slice = pwm_gpio_to_slice_num(LED_PIN_RED); // obter o canal PWM da GPIO
-
-    pwm_clear_irq(slice);                            // Reseta a flag de interrupção para o slice do motor
-    irq_set_exclusive_handler(PWM_IRQ_WRAP, wrapHandlerRED); // interrupção quando o contador do slice atinge o wrap
-    irq_set_enabled(PWM_IRQ_WRAP, true);                  // Habilitar ou desabilitar uma interrupção específica
-
-    pwm_set_clkdiv(slice, PWM_DIVISOR); // define o divisor de clock do PWM
-    pwm_set_wrap(slice, WRAP_PERIOD);   // definir o valor de wrap
-    pwm_set_gpio_level(LED_PIN_RED, 0);     // definir o ciclo de trabalho (duty cycle) do pwm
-    pwm_set_enabled(slice, true);       // habilita o pwm no slice correspondente
-
-    return (slice);
-}
-uint init_PWM_Blue_LED(){
-    // inicializa o pwm do ServoMotor
-
-    gpio_set_function(LED_PIN_BLUE, GPIO_FUNC_PWM);        // habilitar o pino GPIO como PWM
-    uint slice = pwm_gpio_to_slice_num(LED_PIN_BLUE); // obter o canal PWM da GPIO
-
-    pwm_clear_irq(slice);                            // Reseta a flag de interrupção para o slice do motor
-    irq_set_exclusive_handler(PWM_IRQ_WRAP, wrapHandlerBLUE); // interrupção quando o contador do slice atinge o wrap
-    irq_set_enabled(PWM_IRQ_WRAP, true);                  // Habilitar ou desabilitar uma interrupção específica
-
-    pwm_set_clkdiv(slice, PWM_DIVISOR); // define o divisor de clock do PWM
-    pwm_set_wrap(slice, WRAP_PERIOD);   // definir o valor de wrap
-    pwm_set_gpio_level(LED_PIN_BLUE, 0);     // definir o ciclo de trabalho (duty cycle) do pwm
-    pwm_set_enabled(slice, true);       // habilita o pwm no slice correspondente
-
-    return (slice);
+    uint slice_num = pwm_gpio_to_slice_num(gpio);
+    pwm_set_wrap(slice_num, wrap);
+    
+    pwm_set_enabled(slice_num, true);  
+    return slice_num;  
 }
