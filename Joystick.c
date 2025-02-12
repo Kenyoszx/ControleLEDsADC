453c650c6201cd8162bb854bc6af56de5b8ea5c6
João Victor de Souza Silva <joao1452019@gmail.com>
2025-02-12 16:58:32 -0300

    Mudança da Borda e Comentarios

--- Joystick.c
+++ Joystick.c
@@ -22,34 +22,34 @@
 #define LED_PIN_RED 13    // led vermelho
 
 // Constantes
-#define I2C_PORT i2c1
-#define endereco 0x3C
-#define WRAP_PERIOD 4096
-#define STEP 5
+#define I2C_PORT i2c1    // porta i2c
+#define endereco 0x3C    // endereço da porta idc
+#define WRAP_PERIOD 4096 // Periodo de wrap
 
 // Variáveis Globais
-static volatile uint32_t last_time = 0; // Armazena o tempo do último evento (em microssegundos)
-static volatile uint slice_red, slice_blue;
-static volatile bool pwm_state = true;
+static volatile uint32_t last_time = 0;                                    // Armazena o tempo do último evento (em microssegundos)
+static volatile uint slice_red, slice_blue, lr_limit = 125, ud_limit = 60; // Armazena o Numero dos Slices dos LEDs PWM
+static volatile bool pwm_state = true, cor = true,board = true;                                     // Informa se os leds pwm estão ativados
 
 // Protótipos das Funções
-void init();
-void refresh_led_state_X(uint16_t x);
-void refresh_led_state_Y(uint16_t y);
-static void gpio_irq_handler(uint gpio, uint32_t events);
-uint pwm_init_gpio(uint gpio, uint wrap);
+void init();                                              // inicializa o led verde e os botões
+uint pwm_init_gpio(uint gpio, uint wrap);                 // inicializa os LEDs PWM
+void refresh_led_state_X(uint16_t x);                     // Muda o estado do LED vermelho de Acordo com X do Joystick
+void refresh_led_state_Y(uint16_t y);                     // Muda o estado do LED azul de Acordo com Y do Joystick
+static void gpio_irq_handler(uint gpio, uint32_t events); // Trata a interrupção Ao apertar um botão
 
 int main()
 {
     stdio_init_all(); // inicialização entrada e saída
 
     // Variáveis
-    uint16_t adc_value_x;
-    uint16_t adc_value_y;
-    uint16_t pos_x;
-    slice_red = pwm_init_gpio(LED_PIN_RED, WRAP_PERIOD);
-    slice_blue = pwm_init_gpio(LED_PIN_BLUE, WRAP_PERIOD);
-    bool cor = true;
+    uint16_t adc_value_x;                                  // Armazena o valor digital de x
+    uint16_t adc_value_y;                                  // Armazena o valor digital de y
+    uint16_t pos_x;                                        // Armazena a posição de X no display
+    uint16_t pos_y;                                        // Armazena a posição de Y no display
+    slice_red = pwm_init_gpio(LED_PIN_RED, WRAP_PERIOD);   // Inicializa o LED PWM Vermelho
+    slice_blue = pwm_init_gpio(LED_PIN_BLUE, WRAP_PERIOD); // Inicializa o LED Azul
+
 
     init(); // Inicialização dos Pinos
 
@@ -74,18 +74,33 @@ int main()
 
     while (true)
     {
-        adc_select_input(0); // Seleciona o ADC para eixo X. O pino 26 como entrada analógica
-        adc_value_x = adc_read();
-        adc_select_input(1); // Seleciona o ADC para eixo Y. O pino 27 como entrada analógica
-        adc_value_y = adc_read();
-        refresh_led_state_X(adc_value_x);
-        refresh_led_state_Y(adc_value_y);
-        pos_x = ((adc_value_x * -1) + 4095);
-        ssd1306_fill(&ssd, false);
-        ssd1306_rect(&ssd,pos_x/64,adc_value_y/32,8,8,true,true);
-        ssd1306_send_data(&ssd);
+        adc_select_input(0);      // Seleciona o ADC para eixo X. O pino 26 como entrada analógica
+        adc_value_x = adc_read(); // lê o valor de X
+        adc_select_input(1);      // Seleciona o ADC para eixo Y. O pino 27 como entrada analógica
+        adc_value_y = adc_read(); // lê o valor de Y
 
-        sleep_ms(100);
+        refresh_led_state_X(adc_value_x); // Muda o estado do led de acordo com x
+        refresh_led_state_Y(adc_value_y); // Muda o estado do led de acordo com y
+
+        pos_x = (((adc_value_x * -1) + 4095) / 64); // configura a posição X do quadrado no display
+        pos_y = (adc_value_y / 32);                 // configura a posição Y do quadrado no display
+
+        // Ajustes para que o Quadrado não passe da borda
+        if (pos_x > 50)
+            pos_x = 50;
+        else if (pos_x < 8)
+            pos_x = 8;
+        if (pos_y > 115)
+            pos_y = 115;
+        else if (pos_y < 8)
+            pos_y = 8;
+
+        ssd1306_fill(&ssd, !cor);                                // limpa o buffer do display
+        ssd1306_rect(&ssd, pos_x, pos_y, 8, 8, cor, true);        // gera o quadrado 8x8 no buffer
+        ssd1306_rect(&ssd, 3, 3, lr_limit, ud_limit, cor, false); // Desenha um retângulo de borda no Buffer
+        ssd1306_send_data(&ssd);                                  // envia os dados do buffer ao display
+
+        sleep_ms(100); // aguarda 100ms
     }
 }
 
@@ -104,7 +119,7 @@ void refresh_led_state_X(uint16_t x)
     else
         Position = ((Position * -1) + 2048);
 
-    pwm_set_gpio_level(LED_PIN_RED, Position * 2); // define o ciclo ativo (Ton)
+    pwm_set_gpio_level(LED_PIN_BLUE, Position * 2); // define o ciclo ativo (Ton)
 }
 void refresh_led_state_Y(uint16_t y)
 {
@@ -120,20 +135,23 @@ void refresh_led_state_Y(uint16_t y)
     else
         Position = ((Position * -1) + 2048);
 
-    pwm_set_gpio_level(LED_PIN_BLUE, Position * 2); // define o ciclo ativo (Ton)
+    pwm_set_gpio_level(LED_PIN_RED, Position * 2); // define o ciclo ativo (Ton)
 }
 void init()
 {
+    // inicializa o botão do joystick e a interrupção
     gpio_init(JOYSTICK_PB);
     gpio_set_dir(JOYSTICK_PB, GPIO_IN);
     gpio_pull_up(JOYSTICK_PB);
     gpio_set_irq_enabled_with_callback(JOYSTICK_PB, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler); // Rotina de Interrupção
 
+    // inicializa o botão A e a interrupção
     gpio_init(BUTTON_A);
     gpio_set_dir(BUTTON_A, GPIO_IN);
     gpio_pull_up(BUTTON_A);
     gpio_set_irq_enabled_with_callback(BUTTON_A, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler); // Rotina de Interrupção
 
+    // inicializa o LED verde
     gpio_init(LED_PIN_GREEN);
     gpio_set_dir(LED_PIN_GREEN, GPIO_OUT);
     gpio_put(LED_PIN_GREEN, 0);
@@ -152,43 +170,34 @@ static void gpio_irq_handler(uint gpio, uint32_t events)
         // Código Função:
         if (gpio == BUTTON_A)
         {
+            // Altera o estado de Funcionamento dos LEDs PWM
             pwm_state = !pwm_state;
-            pwm_set_enabled(slice_blue,pwm_state);
-            pwm_set_enabled(slice_red,pwm_state);
-            
+            pwm_set_enabled(slice_blue, pwm_state);
+            pwm_set_enabled(slice_red, pwm_state);
         }
         else if (gpio == JOYSTICK_PB)
         {
-            uint count = 0;
+            // Alterna o Estado do LED Verde
             bool state = gpio_get(LED_PIN_GREEN);
+            gpio_put(LED_PIN_GREEN, !state); 
 
-            gpio_put(LED_PIN_GREEN, !state); // Alterna o Estado do LED Verde
-
-            if (count >=3)
-                count = 0;
-            switch (count)
-            {
-            case 0:
-
-                break;
-            case 1:
-
-                break;
-            case 2:
-
-                break;
-            
-            default:
-                printf("[ERRO]");
-                break;
+            // Muda o tamanho da borda e a cor
+            board = !board;
+            if (board){
+                cor = true;
+                lr_limit = 125;
+                ud_limit = 60;
+            }else{
+                cor = false;
+                ud_limit = 58;
+                lr_limit = 122;
             }
-            count ++;
-            
         }
     }
 }
 uint pwm_init_gpio(uint gpio, uint wrap)
 {
+    // Inicializa um LED como PWM
     gpio_set_function(gpio, GPIO_FUNC_PWM);
 
     uint slice_num = pwm_gpio_to_slice_num(gpio);
